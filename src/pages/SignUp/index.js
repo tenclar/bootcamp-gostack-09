65bec1fc4ccccb41f26ff05cf40a1727f9c4d128
Tenclar <tenclarvalus@gmail.com>
2020-01-03 21:31:57 -0500

    autenticacao de usuarios

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,7 +1,46 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
+import logo from '~/assets/logo.svg';
 
-// import { Container } from './styles';
+import { signUpRequest } from '~/store/modules/auth/actions';
+
+const schema = Yup.object().shape({
+  name: Yup.string().required('O nome é obrigatório'),
+  email: Yup.string()
+    .email('Insira um e-mail')
+    .required('O e-email é obrigatório'),
+  password: Yup.string()
+    .min(6, 'No minimo 6 caracteres')
+    .required('A senha é obrigatória'),
+});
 
 export default function SignUp() {
-  return <div />;
+  const dispatch = useDispatch();
+
+  function handleSubmit({ name, email, password }) {
+    dispatch(signUpRequest(name, email, password));
+  }
+
+  return (
+    <>
+      <img src={logo} alt="Gobarber" />
+
+      <Form noValidate schema={schema} onSubmit={handleSubmit}>
+        <Input type="text" name="name" placeholder="Nome Completo" />
+        <Input type="email" name="email" placeholder="Seu e-mail" />
+
+        <Input
+          type="password"
+          name="password"
+          placeholder="Sua senha secreta"
+        />
+        <button type="submit">Criar conta</button>
+
+        <Link to="/">Já tenho login</Link>
+      </Form>
+    </>
+  );
 }
