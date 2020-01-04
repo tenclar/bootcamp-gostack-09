6360e6382ac93a85914e114292a2c786d863dbb9
Tenclar <tenclarvalus@gmail.com>
2020-01-03 21:29:53 -0500

    configuração  caminho com "~"

--- config-overrides.js
+++ config-overrides.js
@@ -1,7 +1,5 @@
-const {addBabelPlugin, override} = require('customize-cra');
+const { addBabelPlugin, override } = require('customize-cra');
+
 module.exports = override(
-  addBabelPlugin([
-    'babel-plugin-root-import',
-    {rootPathSuffix:'src'}
-  ])
-)
+  addBabelPlugin(['babel-plugin-root-import', { rootPathSuffix: 'src' }])
+);
